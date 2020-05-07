1d86b1a9b8c777d6cf5483b68d07239f4b986cd8
manosh7n <manoshin.dm@gmail.com>
2020-05-08 04:46:28 +1000

    HomeScreen visual changes

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -14,8 +14,21 @@ const StackNavigation = () => {
         <Stack.Screen
           name="HomeScreen"
           component={HomeScreen}
-          options={{headerShown: false}}
           initialParams={{country: 'RUS'}}
+          options={{
+            title: 'Covid Tracker',
+            color: 'white',
+            headerStyle: {
+              backgroundColor: '#2964df',
+            },
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              color: 'white',
+              fontFamily: 'Quantico',
+              fontSize: 30,
+            },
+            headerTitleAlign: 'center',
+          }}
         />
         <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
-import {View, Button, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import {Picker} from '@react-native-community/picker';
 
 import {Countries} from '../Components/Countries';
@@ -13,34 +13,94 @@ export default class HomeScreen extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Button
-          title="Details"
-          onPress={() => {
-            this.props.navigation.navigate('Details', {
-              country: this.state.country,
-            });
-          }}
-        />
-        <Picker
-          selectedValue={Object.keys(Countries).find(
-            key => Countries[key] === this.state.country,
-          )}
-          onValueChange={(itemValue, itemIndex) => {
-            this.setState({country: Countries[itemValue]});
-          }}>
-          {Object.keys(Countries).map(i => {
-            return <Picker.Item label={i} value={i} key={i} />;
-          })}
-        </Picker>
-      </View>
+      <>
+        <View style={styles.container}>
+          <View style={styles.pickerView}>
+            <Text style={styles.pickerText}>Select country:</Text>
+            <Picker
+              selectedValue={Object.keys(Countries).find(
+                key => Countries[key] === this.state.country,
+              )}
+              style={styles.picker}
+              onValueChange={(itemValue, itemIndex) => {
+                this.setState({country: Countries[itemValue]});
+              }}>
+              {Object.keys(Countries).map(i => {
+                return <Picker.Item label={i} value={i} key={i} />;
+              })}
+            </Picker>
+          </View>
+
+          <View style={styles.homeStats}>
+            <Text style={styles.textStats}>Infected</Text>
+            <Text style={styles.textStats}>Death</Text>
+          </View>
+        </View>
+
+        <View style={{backgroundColor: 'white'}}>
+          <TouchableOpacity
+            style={styles.button}
+            activeOpacity={0.6}
+            onPress={() => {
+              this.props.navigation.navigate('Details', {
+                country: this.state.country,
+              });
+            }}>
+            <Text style={{fontSize: 18, color: 'white'}}>Details</Text>
+          </TouchableOpacity>
+        </View>
+      </>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'green',
     flex: 1,
+    backgroundColor: 'white',
+  },
+
+  //picker
+  pickerView: {
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+    borderRadius: 15,
+  },
+  picker: {
+    flex: 2,
+    transform: [{scaleX: 1.2}, {scaleY: 1.2}],
+  },
+  pickerText: {
+    flex: 3,
+    textAlign: 'center',
+    fontSize: 18,
+  },
+
+  //stats
+  homeStats: {
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  textStats: {
+    fontSize: 26,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+
+  //button detailed
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#2964df',
+    padding: 10,
+    borderRadius: 20,
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    marginBottom: 10,
   },
 });
